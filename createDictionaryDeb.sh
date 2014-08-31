2f8319519c2f7764152fc463645dce569ecb0aaf
Michał Lenart <michall@ipipan.waw.pl>
2014-08-31 21:00:26 +0000

    - dodanie targetu dictionary-deb (żeby zbudować deba z samym słownikiem)
    git-svn-id: svn://svn.nlp.ipipan.waw.pl/morfeusz/trunk@296 ff4e3ee1-f430-4e82-ade0-24591c43f1fd

---
+++ createDictionaryDeb.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+DICT_DIR="$1"
+DICT_NAME="$2"
+DICT_VERSION="$3"
+TARGET_DIR="$4"
+
+PKG_NAME="morfeusz-dictionary-${DICT_NAME}"
+
+TMP_DIR=`mktemp -d`
+PKG_DIR="$TMP_DIR"/"$PKG_NAME"_"$DICT_VERSION"
+TARGET_DICT_DIR="$PKG_DIR/usr/share/morfeusz/dictionaries"
+mkdir -p "$TARGET_DICT_DIR"
+cp "$DICT_DIR/$DICT_NAME-a.dict" "$TARGET_DICT_DIR"
+cp "$DICT_DIR/$DICT_NAME-s.dict" "$TARGET_DICT_DIR"
+mkdir "$PKG_DIR/DEBIAN"
+
+CONTROL_FILE="$PKG_DIR/DEBIAN/control"
+
+cat >"$CONTROL_FILE" <<EOL
+Package: morfeusz-dictionary-${DICT_NAME}
+Version: ${DICT_VERSION}
+Section: devel
+Priority: optional
+Architecture: all
+Maintainer: Michał Lenart <michal.lenart@ipipan.waw.pl>
+Depends: morfeusz2
+Description: ${DICT_NAME} dictionary for Morfeusz.
+EOL
+
+chmod 0644 "$CONTROL_FILE"
+
+dpkg-deb --build "$PKG_DIR" "$TARGET_DIR"
