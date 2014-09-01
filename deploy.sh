1e3f9cbd0cd9a26b12cf3e361ccdbd82cf79bab6
Michał Lenart <michall@ipipan.waw.pl>
2014-09-01 15:25:14 +0000

    - mała poprawka w deploy.sh
    git-svn-id: svn://svn.nlp.ipipan.waw.pl/morfeusz/trunk@302 ff4e3ee1-f430-4e82-ade0-24591c43f1fd

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tar -czf - target/ | ssh mlenart@chopin.ipipan.waw.pl "cd public_html && rm -rf morfeusz target && tar -xzf - && mv target morfeusz"
+tar -czf - target/ | ssh mlenart@chopin.ipipan.waw.pl "cd public_html && rm -rf morfeusz target && tar -xzf - && mv target morfeusz && chmod a+x morfeusz"
